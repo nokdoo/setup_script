5cd779656fceeb1b90a6783135ed55247e5ebf7d
nokdoot <nokdoot@naver.com>
2019-04-11 01:09:20 +0900

    freebsd default setting

---
+++ freebsd/setup.pl
@@ -0,0 +1,18 @@
+#!/usr/env/bin perl
+
+use strict;
+use warnings;
+
+system("echo 'allscreens_kbdflags=\"YES\"' >> /etc/rc.conf");
+
+system("pkg install -y sudo");
+system("echo 'nokdoot ALL=(ALL) ALL' >> /etc/sudoers");
+
+system("pkg install -y xorg");
+
+system("pkg install -y i3 i3lock i3status dmenu");
+
+system("echo '/usr/local/bin/i3' >> /usr/home/nokdoot/.xinitrc");
+
+system("echo 'hald_enable=\"YES\"' >> /etc/rc.conf");
+system("echo 'dbus_enable=\"YES\"' >> /etc/rc.conf");
