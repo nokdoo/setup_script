82175bae479464db096c2a7aeef3931d704e1d71
nokdoot <nokdoot@naver.com>
2019-04-12 18:09:11 +0900

    freebsd

--- freebsd/setup.pl
+++ freebsd/setup.pl
@@ -6,13 +6,16 @@ use warnings;
 system("echo 'allscreens_kbdflags=\"-b quiet.off\"' >> /etc/rc.conf");
 
 system("pkg install -y sudo");
-system("echo 'nokdoot ALL=(ALL) ALL' >> /etc/sudoers");
 
 system("pkg install -y xorg");
 
 system("pkg install -y i3 i3lock i3status dmenu");
 
-system("echo '/usr/local/bin/i3' >> /usr/home/nokdoot/.xinitrc");
+system("echo 'exec /usr/local/bin/i3' >> /usr/home/nokdoot/.xinitrc");
+system("pkg install -y x11/slim");
 
 system("echo 'hald_enable=\"YES\"' >> /etc/rc.conf");
 system("echo 'dbus_enable=\"YES\"' >> /etc/rc.conf");
+
+print "     Check visudo\n";
+print "     Check /etc/ttys off->on\n";
