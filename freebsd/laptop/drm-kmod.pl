0925fa42e34f0b1555c6038e52967008df9cbba9
Charlie Root <nokdoot@naver.com>
2019-04-20 10:31:37 +0900

    config and i3-new-workspace

---
+++ freebsd/laptop/drm-kmod.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+system "pkg install -y drm-kmod";
+
+system "echo kld_list=\"/boot/modules/i915kms.ko\" >> /etc/rc.conf";
+
+system "pw usermod nokdoot -G video";
+
+print "		reboot!!\n";
+
